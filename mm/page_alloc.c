4c63fc124465aa5b0b7c7b1fa41670b26646f2f5
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2013-02-22 16:32:09 -0800

    CMA: make putback_lru_pages() call conditional
    As per documentation and other places calling putback_lru_pages(),
    putback_lru_pages() is called on error only.  Make the CMA code behave
    consistently.
    
    [akpm@linux-foundation.org: remove a test-n-branch in the wrapup code]
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    
    	mm/page_alloc.c

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5868,9 +5868,11 @@ static int __alloc_contig_migrate_range(struct compact_control *cc,
 				    __alloc_contig_migrate_alloc,
 				    0, false, MIGRATE_SYNC);
 	}
-
-	putback_lru_pages(&cc->migratepages);
-	return ret > 0 ? 0 : ret;
+	if (ret < 0) {
+		putback_lru_pages(&cc->migratepages);
+		return ret;
+	}
+	return 0;
 }
 
 /**
