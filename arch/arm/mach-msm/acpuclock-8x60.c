e058f4a75e500bbe29428159f22836ee8a7434ae
mickybart <mickybart@pygoscelis.org>
2014-12-12 13:27:18 -0500

    Revert "overclocking: fix max/min cpu frequencies on the CPU during boot"
    This reverts commit 39fdfc7496ddc7d47df92b3f7414a29ec7ee0e51.
    
    Due to a "bug" when cpu come back online from offline state
    the max/min are not take into account so the core can run in an
    unsupported frequency

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -1153,11 +1153,7 @@ static int __init acpuclk_8x60_probe(struct platform_device *pdev)
 
 	/* Improve boot time by ramping up CPUs immediately. */
 	for_each_online_cpu(cpu)
-#ifndef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
 		acpuclk_8x60_set_rate(cpu, max_freq->acpuclk_khz, SETRATE_INIT);
-#else
-		acpuclk_8x60_set_rate(cpu, CONFIG_MSM_CPU_FREQ_MAX, SETRATE_INIT);
-#endif
 
 	acpuclk_register(&acpuclk_8x60_data);
 	cpufreq_table_init();
