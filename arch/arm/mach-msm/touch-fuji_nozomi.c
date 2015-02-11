45a2cef903abe354b00eade7e9c8a122666307ea
mickybart <mickybart@pygoscelis.org>
2015-02-10 21:02:40 -0500

    Revert "clearpad: change key MENU to APP_SWITCH"
    This reverts commit 24b1d96db8508a81d91ab7f0762e0b297e07454b.

--- arch/arm/mach-msm/touch-fuji_nozomi.c
+++ arch/arm/mach-msm/touch-fuji_nozomi.c
@@ -13,8 +13,6 @@
 #include <linux/input.h>
 #include <linux/clearpad.h>
 
-#define KEY_APP_SWITCH 580 /* Android specific key */
-
 struct synaptics_button_data synaptics_back_key = {
 	.code = KEY_BACK,
 };
@@ -23,8 +21,8 @@ struct synaptics_button_data synaptics_home_key = {
 	.code = KEY_HOME,
 };
 
-struct synaptics_button_data synaptics_app_switch_key = {
-	.code = KEY_APP_SWITCH,
+struct synaptics_button_data synaptics_menu_key = {
+	.code = KEY_MENU,
 };
 
 struct synaptics_funcarea clearpad_funcarea_array[] = {
@@ -42,7 +40,7 @@ struct synaptics_funcarea clearpad_funcarea_array[] = {
 	},
 	{
 		{ 500, 1315, 719, 1327 }, { 480, 1280, 719, 1327 },
-		SYN_FUNCAREA_BUTTON, &synaptics_app_switch_key
+		SYN_FUNCAREA_BUTTON, &synaptics_menu_key
 	},
 	{
 		{ 240, 460, 479, 819 }, { 240, 460, 479, 819 },
