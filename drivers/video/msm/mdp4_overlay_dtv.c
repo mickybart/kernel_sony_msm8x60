6a85e00a0aadc0dddf1336f6cdc9c1e9db5e79c4
cb <billchen1977@gmail.com>
2015-03-05 14:35:06 +0800

    Revert "msm: mdp: Wait unconditionally in internal vsync waits"
    This reverts commit e96b3d9567496115fcb7e0057e77f15030f8bd71.

--- drivers/video/msm/mdp4_overlay_dtv.c
+++ drivers/video/msm/mdp4_overlay_dtv.c
@@ -354,6 +354,7 @@ void mdp4_dtv_wait4vsync(int cndx)
 {
 	struct vsycn_ctrl *vctrl;
 	struct mdp4_overlay_pipe *pipe;
+	ktime_t timestamp;
 
 	if (cndx >= MAX_CONTROLLER) {
 		pr_err("%s: out or range: cndx=%d\n", __func__, cndx);
@@ -368,7 +369,10 @@ void mdp4_dtv_wait4vsync(int cndx)
 
 	mdp4_dtv_vsync_irq_ctrl(cndx, 1);
 
-	wait_event_interruptible_timeout(vctrl->wait_queue, 1,
+	timestamp = vctrl->vsync_time;
+	wait_event_interruptible_timeout(vctrl->wait_queue,
+			!ktime_equal(timestamp, vctrl->vsync_time)&&
+			vctrl->vsync_irq_enabled,
 			msecs_to_jiffies(VSYNC_PERIOD * 8));
 
 	mdp4_dtv_vsync_irq_ctrl(cndx, 0);
