adb9f1b40ccaecb5bc38b804eba7a3315928a3ed
mickybart <mickybart@pygoscelis.org>
2015-08-21 20:57:30 -0400

    fs: btrfs: fix warning compilation with cast to ulong

--- fs/btrfs/file-item.c
+++ fs/btrfs/file-item.c
@@ -29,7 +29,7 @@
 				   sizeof(struct btrfs_item) * 2) / \
 				  size) - 1))
 
-#define MAX_CSUM_ITEMS(r, size) (min(__MAX_CSUM_ITEMS(r, size), PAGE_CACHE_SIZE))
+#define MAX_CSUM_ITEMS(r, size) (min(((ulong)(__MAX_CSUM_ITEMS(r, size))), PAGE_CACHE_SIZE))
 
 #define MAX_ORDERED_SUM_BYTES(r) ((PAGE_SIZE - \
 				   sizeof(struct btrfs_ordered_sum)) / \
