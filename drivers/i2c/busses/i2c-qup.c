8bc4f16b769ad92eaea121f64442e6dda04ab4d9
Bill Chen <billchen1977@gmail.com>
2016-01-31 23:22:44 +0800

    qup_i2c: use noirq so that we suspend late and resume before the wakeup interrupt

--- drivers/i2c/busses/i2c-qup.c
+++ drivers/i2c/busses/i2c-qup.c
@@ -1501,15 +1501,10 @@ static int qup_i2c_resume(struct device *device)
 #endif /* CONFIG_PM */
 
 static const struct dev_pm_ops i2c_qup_dev_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(
-		qup_i2c_suspend,
-		qup_i2c_resume
-	)
-	SET_RUNTIME_PM_OPS(
-		i2c_qup_pm_suspend_runtime,
-		i2c_qup_pm_resume_runtime,
-		NULL
-	)
+	.suspend_noirq		= qup_i2c_suspend,
+	.resume_noirq		= qup_i2c_resume,
+	.runtime_suspend	= i2c_qup_pm_suspend_runtime,
+	.runtime_resume		= i2c_qup_pm_resume_runtime,
 };
 
 static struct of_device_id i2c_qup_dt_match[] = {
